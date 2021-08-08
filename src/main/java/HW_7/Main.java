076e4d3672dd6eb96fab44dd3d0072d5c88857d3
Dotochin <Dotochin21@gmail.com>
2021-08-09 06:24:53 +0700

    Читабельный вывод текущей погоды.

---
+++ src/main/java/HW_7/Main.java
@@ -0,0 +1,8 @@
+package HW_7;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+        userInterfaceView.runInterface();
+    }
+}
